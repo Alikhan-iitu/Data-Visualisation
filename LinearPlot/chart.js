9a1b446ae9475720aa02df550bae50cddaadfcc5
Alikhan-iitu <60458631+Alikhan-iitu@users.noreply.github.com>
2022-10-08 01:08:40 +0600

    Update chart.js

--- LinearPlot/chart.js
+++ LinearPlot/chart.js
@@ -5,6 +5,7 @@ async function buildPlot() {
     const dateParser = d3.timeParse("%Y-%m-%d");
     const yAccessor = (d) => d.temperatureMin;
     const xAccessor = (d) => dateParser(d.date);
+
     // Функции для инкапсуляции доступа к колонкам набора данных
 
     var dimension = {
@@ -27,14 +28,25 @@ async function buildPlot() {
     svg.attr("width",dimension.width);
     const bounded = svg.append("g");
     bounded.style("transform",`translate(${dimension.margin.left}px, ${dimension.margin.top})`);
+    var height = 450;
 
     const yScaler = d3.scaleLinear()
         .domain(d3.extent(data,yAccessor))
         .range([dimension.boundedHeight,0]);
+    svg.append("g")
+        .attr("transform", "translate(15, 15)")
+        .call(d3.axisLeft(yScaler));
 
     const xScaler = d3.scaleTime()
         .domain(d3.extent(data,xAccessor))
         .range([0,dimension.boundedWidth]);
+    svg.append("g")
+        .attr("transform", "translate(0," + dimension.boundedHeight + ")")
+        .call(d3.axisBottom(xScaler));
+
+
+    // var yAxis = d3.yAxisLeft(yAccessor);
+    // var xAxis = d3.xAxisBottom(xAccessor);
 
     var lineGenerator = d3.line()
         .x(d => xScaler(xAccessor(d)))
@@ -43,7 +55,9 @@ async function buildPlot() {
     bounded.append("path")
         .attr("d",lineGenerator(data))
         // .attr("fill","none")
-        .attr("stroke","lightgrey")
+        .attr("stroke","lightgrey");
+    // bounded.append('g').attr('class', 'x axis').attr("transform", "translate(0, "+height+")").call(xAxis);
+    // bounded.append('g').attr('class', 'y axis').call(yAxis);
 
 }
 
